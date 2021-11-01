5be10f210635d7cfe54f8a8e417de9978fe5c284
jeffeotoni <jeff.otoni@gmail.com>
2021-11-01 01:16:31 -0300

    rename project registry

--- deploy.gcloud.sh
+++ deploy.gcloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "... build Google Cloud Artifacts Registry.."
-docker build -f Dockerfile -t us-central1-docker.pkg.dev/beta-teste/go2/gcloudrun.user .
-docker push us-central1-docker.pkg.dev/beta-teste/go2/gcloudrun.user:latest
+docker build -f Dockerfile -t us-central1-docker.pkg.dev/<your-project>/<name-repo>/gcloudrun.user .
+docker push us-central1-docker.pkg.dev/<your-project>/<name-repo>/gcloudrun.user:latest
 echo "... push success .."
